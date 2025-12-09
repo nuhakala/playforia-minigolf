99d52ceb8ad7e680b608fc9175d6adefe8589dc5
nuhakala <95333914+nuhakala@users.noreply.github.com>
2025-12-09 17:08:18 +0200

    Refactor: create separate class for gamestate

---
+++ client/src/main/java/agolf/game/GameState.java
@@ -0,0 +1,48 @@
+package agolf.game;
+
+import java.util.List;
+import agolf.Seed;
+import agolf.SynchronizedBool;
+
+public class GameState {
+
+    // Constants
+    private static final double MAGIC_OFFSET = Math.sqrt(2.0) / 2.0;
+    private static final int DIAG_OFFSET = (int) (6.0 * MAGIC_OFFSET + 0.5);
+    private static final int[] FRAME_TIME_HISTORY = new int[] { Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE };
+
+    // Game state variables
+    public int playerCount;
+    public int onShoreSetting;
+    public int collisionMode;
+    public int currentPlayerId;
+    public int isValidPlayerId;
+    public int gameState;
+
+    public double startPositionX;
+    public double startPositionY;
+    public double bounciness;
+    public double somethingSpeedThing;
+
+    public double[] resetPositionX;
+    public double[] resetPositionY;
+
+    public List<double[]>[] teleportStarts;
+    public List<double[]>[] teleportExits;
+
+    public short[][][] magnetMap;
+
+    public double[] playerX;
+    public double[] playerY;
+    public double[] speedX;
+    public double[] speedY;
+
+    public boolean[] simulatePlayer;
+    public SynchronizedBool[] onHoleSync;
+    public boolean isLocalPlayer;
+    public boolean[] playerActive;
+
+    public Seed seed;
+    public int maxPhysicsIterations;
+    public boolean strokeInterrupted;
+}
